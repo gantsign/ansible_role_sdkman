43acb04a6444cddd51e7ba745807a0a47fd3349e
John Freeman <john.freeman.opensource@gmail.com>
2020-10-10 17:02:52 +0100

    Updated Molecule to 3.0.8
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,13 +1,5 @@
 import pytest
-
-import os
-
-import testinfra.utils.ansible_runner
-
 import re
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 @pytest.mark.parametrize('dir_path', [
